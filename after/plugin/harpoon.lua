a8e76454a6347afd55c61536a6b00630798fd9be
Krishna Shah <kshahusa@gmail.com>
2024-04-14 17:29:34 -0500

    add tabline with harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,12 +1,38 @@
+require("harpoon").setup(
+    {
+    -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+    save_on_toggle = false,
 
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+    -- saves the harpoon file upon every change. disabling is unrecommended.
+    save_on_change = true,
 
-vim.keymap.set("n", "<leader>ha", mark.add_file)
-vim.keymap.set("n", "<leader>hs", ui.toggle_quick_menu)
+    -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+    enter_on_sendcmd = false,
 
+    -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+    tmux_autoclose_windows = false,
 
-vim.keymap.set("n", "<leader>h1", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>h2", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<leader>h3", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<leader>h4", function() ui.nav_file(4) end)
+    -- filetypes that you want to prevent from adding to the harpoon list menu.
+    excluded_filetypes = { "harpoon" },
+
+    -- set marks specific to each git branch inside git repository
+    mark_branch = false,
+
+    -- enable tabline with harpoon marks
+    tabline = true,
+    tabline_prefix = "   ",
+    tabline_suffix = "   ",
+}
+)
+
+-- local mark = require("harpoon.mark")
+-- local ui = require("harpoon.ui")
+--
+-- vim.keymap.set("n", "<leader>ha", mark.add_file)
+-- vim.keymap.set("n", "<leader>hs", ui.toggle_quick_menu)
+--
+--
+-- vim.keymap.set("n", "<leader>h1", function() ui.nav_file(1) end)
+-- vim.keymap.set("n", "<leader>h2", function() ui.nav_file(2) end)
+-- vim.keymap.set("n", "<leader>h3", function() ui.nav_file(3) end)
+-- vim.keymap.set("n", "<leader>h4", function() ui.nav_file(4) end)
--- lua/krishna/remap.lua
+++ lua/krishna/remap.lua
@@ -36,6 +36,18 @@ vim.keymap.set('n', '<leader>c', ':TagbarToggle<CR>')
 -- TPOPE COMMENTARY vim.keymap.set('n', '<C-/>', ':Commentary<CR>')
 
 
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>ha", mark.add_file)
+vim.keymap.set("n", "<leader>hs", ui.toggle_quick_menu)
+
+
+vim.keymap.set("n", "<leader>h1", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>h2", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>h3", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>h4", function() ui.nav_file(4) end)
+
 
 vim.api.nvim_set_keymap('n', '<leader>ccq', [[:lua << EOF
     local input = vim.fn.input("Quick Chat: ")
