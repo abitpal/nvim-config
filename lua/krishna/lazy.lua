56c0ecf799785beea0bd8f20f6b40f7d07c4e4e2
Krishna Shah <kshahusa@hmail.com>
2024-06-18 20:08:38 -0500

    added startify

--- lua/krishna/lazy.lua
+++ lua/krishna/lazy.lua
@@ -291,13 +291,7 @@ require("lazy").setup({
                 { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
             },
         },
-        {
-            "folke/persistence.nvim",
-            event = "BufReadPre", -- this will only start session saving when an actual file was opened
-            opts = {
-                -- add any custom options here
-            }
-        }
+        'mhinz/vim-startify'
     }
 })
 
--- lua/krishna/remap.lua
+++ lua/krishna/remap.lua
@@ -20,17 +20,6 @@ vim.keymap.set({"n","x"}, "gP", "<Plug>(YankyGPutBefore)")
 vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
 vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
 
-
--- restore the session for the current directory
-vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], {})
-
--- restore the last session
-vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
-
--- stop Persistence => session won't be saved on exit
-vim.api.nvim_set_keymap("n", "<leader>qd", [[<cmd>lua require("persistence").stop()<cr>]], {})
-
-
 xnoremap("<", "<gv")
 xnoremap(">", ">gv")
 --vim.keymap.set("n", "<F5>", ":sp<bar>term<cr><c-w>J:resize10<cr>i")
